4a3f4941d572c19b7549e6b43e34b00e57b3d6e6
H4K3R13 <mit2224366@gmail.com>
2023-02-06 23:22:18 +0530

    Updated Client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from "./components/SignUp";
+function App() {
+  return (
+    <div className="App">
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
+
+
