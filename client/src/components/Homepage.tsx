75208cc0ae249e876458b9a98ff8ae2ebd5d5377
H4K3R13 <mit2224366@gmail.com>
2023-02-07 09:13:29 +0530

    Updating Client added react-route

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -3,7 +3,10 @@ import { Stack } from "@mui/system"
 
 export const Homepage = () => {
   return (
-    <Stack spacing={5} direction="row" alignItems="center">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    <Stack spacing={5} direction="row" display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="100vh">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         <Button variant="contained">Sign In</Button>
         <Button variant="contained">Sign Up</Button>
     </Stack>
