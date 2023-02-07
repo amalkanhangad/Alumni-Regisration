d10fca8f68f95b7852222f84eef2f1b52e3a1dba
H4K3R13 <mit2224366@gmail.com>
2023-02-07 15:29:56 +0530

    Edited HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import  SignUp  from './components/SignUp'
 import SignIn from './components/SignIn'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <SignUp/>
-      <SignIn/>
     </div>
   );
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -20,8 +20,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://nasc.ac.in/">
+        NASC
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -29,7 +29,7 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4,];
 
 const theme = createTheme();
 
@@ -56,18 +56,17 @@ export default function Album() {
         >
           <Container maxWidth="sm">
             <Typography
+              fontFamily="SF Pro"
               component="h1"
               variant="h2"
               align="center"
               color="text.primary"
               gutterBottom
             >
-              Album layout
+              NASC Alumni
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
+            The association by the former students of the college functions as a part of their obligation to the Alma mater. Various activities are being conducted to keep the college to achieve its goals. The association has been taking up various ventures for the development of the college.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -75,12 +74,12 @@ export default function Album() {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
+              <Button variant="contained">Register</Button>
+              <Button variant="outlined">Login</Button>
             </Stack>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
+       <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
@@ -114,21 +113,10 @@ export default function Album() {
               </Grid>
             ))}
           </Grid>
-        </Container>
+                  </Container>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
+      <Box sx={{ bgcolor: 'background.paper', p: 40 }} component="footer">
         <Copyright />
       </Box>
       {/* End footer */}
